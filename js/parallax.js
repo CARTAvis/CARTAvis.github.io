7907433db83b3689a77a39a03e24ed1402df7c7e
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2020-09-17 10:23:03 +0800

    removing unused test .js files

--- js/parallax.js
+++
@@ -1,7 +0,0 @@
-//Call in document load event
-document.getElementById("banner")
-.addEventListener('mousemove', function(e) {
-  var x = -(e.pageX + this.offsetLeft) / 50;
-  var y = -(e.pageY + this.offsetTop) / 50;
-  e.currentTarget.style.backgroundPosition = x + 'px ' + y + 'px';
-})
